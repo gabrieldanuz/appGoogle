aa2414320e613b548f9320e025e32398bbc793f3
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2022-12-07 19:06:45 -0300

    Começando

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Lato_400Regular,
+  Lato_700Bold
+} from '@expo-google-fonts/lato'
+import Loading from 'expo-app-loading'
+import { Text, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) return <Loading />
+
+  return (
+    <View>
+      <Text>Carregou as fontes</Text>
+    </View>
+  )
+}
