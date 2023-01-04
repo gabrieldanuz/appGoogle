a3ed7f196c20e59b44eb8fc487e1477b64cb24bb
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2023-01-04 18:46:44 -0300

    Lottie load

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
+import { Load } from '../components/Load'
 
 export function Home() {
+  const [load, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+  })
+
+  if (load) return <Load />
+
   return (
     <View>
       <Text>Home</Text>
